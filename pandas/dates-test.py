934aea2962ea40a3303be4bfcc96335b7c74fb67
orbenor <orbenor@gmail.com>
2022-01-28 00:59:01 +0200

    Best time format solution from codegrepper.com

---
+++ pandas/dates-test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+date='2020/11/26 12:00:00'
+date_time=pd.to_datetime(date, format='%Y/%m/%d %H:%M:%S')
+print(date_time)
+
+date='2020/11/26 12:00:00'
+date_time=pd.to_datetime(date, format='%Y/%m/%d %H:%M:%S')
+print(date_time.strftime('%d-%m-%Y'))
